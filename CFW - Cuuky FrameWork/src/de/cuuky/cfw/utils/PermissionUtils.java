71f3bec5633e3d41376449987a602a59f5e54bbf
CuukyOfficial <just.cookie.jc@gmail.com>
2020-04-25 11:01:09 +0200

    Fixed LuckPerms Prefix getter

--- CFW - Cuuky FrameWork/src/de/cuuky/cfw/utils/PermissionUtils.java
+++ CFW - Cuuky FrameWork/src/de/cuuky/cfw/utils/PermissionUtils.java
@@ -2,10 +2,17 @@ package de.cuuky.cfw.utils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 import de.cuuky.cfw.player.CustomPlayer;
+import net.luckperms.api.LuckPerms;
 
 public final class PermissionUtils {
+
+	private static Object luckPermsAPI, userManager, groupManager, queryOptions;
 
 	private static HashMap<String, Class<?>> clazzes;
 
@@ -18,35 +25,46 @@ public final class PermissionUtils {
 		} catch (Exception e) {}
 
 		try {
-			toAdd.add(Class.forName("me.lucko.luckperms.LuckPerms"));
-			toAdd.add(Class.forName("me.lucko.luckperms.api.Contexts"));
+			toAdd.add(Class.forName("net.luckperms.api.LuckPerms"));
+			toAdd.add(Class.forName("net.luckperms.api.query.QueryOptions"));
 		} catch (Exception e) {}
 
 		for (Class<?> clazz : toAdd)
 			clazzes.put(clazz.getName(), clazz);
+
+		try {
+			RegisteredServiceProvider<?> provider = Bukkit.getServicesManager().getRegistration(clazzes.get("net.luckperms.api.LuckPerms"));
+			luckPermsAPI = null;
+
+			if (provider != null)
+				luckPermsAPI = (LuckPerms) provider.getProvider();
+
+			userManager = luckPermsAPI.getClass().getMethod("getUserManager").invoke(luckPermsAPI);
+			groupManager = luckPermsAPI.getClass().getMethod("getGroupManager").invoke(luckPermsAPI);
+			queryOptions = clazzes.get("net.luckperms.api.query.QueryOptions").getDeclaredMethod("defaultContextualOptions").invoke(null);
+		} catch (Exception e) {}
 	}
 
 	public static String getLuckPermsPrefix(CustomPlayer player) {
 		try {
-			Object api = clazzes.get("me.lucko.luckperms.LuckPerms").getDeclaredMethod("getApi").invoke(null);
+			Object user = userManager.getClass().getMethod("getUser", UUID.class).invoke(userManager, UUID.fromString(player.getUUID()));
+			String groupname = (String) user.getClass().getMethod("getPrimaryGroup").invoke(user);
 
-			Object user = api.getClass().getMethod("getUser", String.class).invoke(api, player.getUUID());
-			String groupname = (String) user.getClass().getMethod("getPrimaryGroup").invoke(api);
-
-			Object group = api.getClass().getMethod("getGroup", String.class).invoke(api, groupname);
+			Object group = groupManager.getClass().getMethod("getGroup", String.class).invoke(groupManager, groupname);
 			Object cachedData = group.getClass().getMethod("getCachedData").invoke(group);
 
-			Object contexts = clazzes.get("me.lucko.luckperms.api.Contexts").getDeclaredMethod("allowAll").invoke(null);
-
-			Object metadata = cachedData.getClass().getMethod("getMetaData", contexts.getClass()).invoke(cachedData, contexts);
-			return (String) metadata.getClass().getMethod("getPrefix").invoke(metadata);
-		} catch (Throwable e) {}
+			Object metadata = cachedData.getClass().getMethod("getMetaData", clazzes.get("net.luckperms.api.query.QueryOptions")).invoke(cachedData, queryOptions);
+			String prefix = (String) metadata.getClass().getMethod("getPrefix").invoke(metadata);
+			return prefix != null ? prefix.replace("&", "§") : prefix;
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
 
 		return "";
 	}
 
 	public static String getPermissionsExPrefix(CustomPlayer player) {
-		String prefix = "";
+		String prefix = null;
 		try {
 			Object permissionUser = clazzes.get("ru.tehkode.permissions.bukkit.PermissionsEx").getDeclaredMethod("getUser", String.class).invoke(null, player.getName());
 			Object[] groups = ((Object[]) permissionUser.getClass().getDeclaredMethod("getGroups").invoke(permissionUser));
@@ -55,10 +73,8 @@ public final class PermissionUtils {
 				prefix = (String) groups[0].getClass().getMethod("getPrefix").invoke(groups[0]);
 			else
 				prefix = (String) permissionUser.getClass().getMethod("getPrefix").invoke(permissionUser);
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
+		} catch (Throwable e) {}
 
-		return prefix.replace("&", "�");
+		return prefix != null ? prefix.replace("&", "§") : prefix;
 	}
 }
