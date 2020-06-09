3d10fd124106682b57e241b63efe80490872ba8e
CuukyOfficial <just.cookie.jc@gmail.com>
2020-06-09 20:20:35 +0200

    Fixed null for tab header and footer

--- CFW - Cuuky FrameWork/src/de/cuuky/cfw/clientadapter/board/tablist/CustomTablist.java
+++ CFW - Cuuky FrameWork/src/de/cuuky/cfw/clientadapter/board/tablist/CustomTablist.java
@@ -32,24 +32,27 @@ public class CustomTablist extends CustomBoard {
 	private Object[] updateList(CustomPlayer player, boolean header) {
 		ArrayList<String> tablistLines = header ? this.getUpdateHandler().getTablistHeader(player.getPlayer()) : this.getUpdateHandler().getTablistFooter(player.getPlayer()), oldList = null;
 
-		if (header) {
+		if(header) {
 			oldList = headerReplaces.get(player.getPlayer());
-			if (oldList == null)
+			if(oldList == null)
 				headerReplaces.put(player.getPlayer(), oldList = new ArrayList<>());
 		} else {
 			oldList = footerReplaces.get(player.getPlayer());
-			if (oldList == null)
+			if(oldList == null)
 				footerReplaces.put(player.getPlayer(), oldList = new ArrayList<>());
 		}
 
+		if(tablistLines == null)
+			return new Object[] { false };
+
 		boolean changed = false;
-		for (int index = 0; index < tablistLines.size(); index++) {
+		for(int index = 0; index < tablistLines.size(); index++) {
 			String line = tablistLines.get(index);
 
-			if (oldList.size() < tablistLines.size()) {
+			if(oldList.size() < tablistLines.size()) {
 				oldList.add(line);
 				changed = true;
-			} else if (!oldList.get(index).equals(line)) {
+			} else if(!oldList.get(index).equals(line)) {
 				oldList.set(index, line);
 				changed = true;
 			}
@@ -64,18 +67,17 @@ public class CustomTablist extends CustomBoard {
 		String tablistname = getUpdateHandler().getTablistName(player.getPlayer());
 
 		int maxlength = BukkitVersion.ONE_8.isHigherThan(VersionUtils.getVersion()) ? 16 : -1;
-		if (maxlength > 0)
-			if (tablistname.length() > maxlength)
+		if(maxlength > 0)
+			if(tablistname.length() > maxlength)
 				tablistname = this.player.getName();
 
-		if (this.tabname == null || !this.tabname.equals(tablistname))
+		if(this.tabname == null || !this.tabname.equals(tablistname))
 			player.getPlayer().setPlayerListName(this.tabname = tablistname);
 
 		Object[] headerUpdate = updateList(player, true);
 		Object[] footerUpdate = updateList(player, false);
 
-		if ((boolean) headerUpdate[0] || (boolean) footerUpdate[0]) {
+		if((boolean) headerUpdate[0] || (boolean) footerUpdate[0])
 			player.getNetworkManager().sendTablist(JavaUtils.getArgsToString((ArrayList<String>) headerUpdate[1], "\n"), JavaUtils.getArgsToString((ArrayList<String>) footerUpdate[1], "\n"));
-		}
 	}
 }
