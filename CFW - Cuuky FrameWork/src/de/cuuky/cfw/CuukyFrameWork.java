2c7890372aa658fa1ae9c3705f8237fb12966c70
CuukyOfficial <just.cookie.jc@gmail.com>
2020-07-08 16:12:07 +0200

    Updated version to 0.3

--- CFW - Cuuky FrameWork/src/de/cuuky/cfw/CuukyFrameWork.java
+++ CFW - Cuuky FrameWork/src/de/cuuky/cfw/CuukyFrameWork.java
@@ -20,7 +20,7 @@ public class CuukyFrameWork<T extends CustomPlayer> {
 	/*
 	 * CFW - A Bukkit framework
 	 * 
-	 * VERSION: 0.2 (2020) AUTHOR: Cuuky CONTACT: { website: "varoplugin.de",
+	 * VERSION: 0.3 (2020) AUTHOR: Cuuky CONTACT: { website: "varoplugin.de",
 	 * discord: 'Cuuky#2783', mail: 'just.cookie.jc@gmail.com' }
 	 */
 
