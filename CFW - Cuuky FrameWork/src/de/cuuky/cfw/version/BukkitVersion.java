e99e5301367113cdebed03222cde6a992a3a6a23
CuukyOfficial <just.cookie.jc@gmail.com>
2020-07-12 16:33:04 +0200

    Fixed getting Version and added more 1.16 support

--- CFW - Cuuky FrameWork/src/de/cuuky/cfw/version/BukkitVersion.java
+++ CFW - Cuuky FrameWork/src/de/cuuky/cfw/version/BukkitVersion.java
@@ -1,5 +1,9 @@
 package de.cuuky.cfw.version;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
 public enum BukkitVersion {
 
 	ONE_10(10),
@@ -8,6 +12,7 @@ public enum BukkitVersion {
 	ONE_13(13),
 	ONE_14(14),
 	ONE_15(15),
+	ONE_16(16),
 	ONE_7(7),
 	ONE_8(8),
 	ONE_9(9);
@@ -26,16 +31,24 @@ public enum BukkitVersion {
 		return this.identifier;
 	}
 
-	public static BukkitVersion getVersion(String v) {
-		int versionNumber = Integer.valueOf(v.split("1_")[1].split("_")[0]);
-		for (BukkitVersion version : values())
+	public static BukkitVersion getVersion(String versionString) {
+		Map<Integer, BukkitVersion> sorted = new TreeMap<Integer, BukkitVersion>();
+		int versionNumber = Integer.valueOf(versionString.split("1_")[1].split("_")[0]);
+		for (BukkitVersion version : values()) {
 			if (versionNumber == version.identifier)
 				return version;
 
-		if (versionNumber < values()[1].identifier)
-			return values()[0];
-		else if (versionNumber > values()[values().length - 2].identifier)
-			return values()[values().length - 1];
+			sorted.put(version.getIdentifier(), version);
+		}
+
+		ArrayList<BukkitVersion> inOrder = new ArrayList<BukkitVersion>();
+		for (int i : sorted.keySet())
+			inOrder.add(sorted.get(i));
+
+		if (versionNumber < inOrder.get(1).getIdentifier())
+			return inOrder.get(0);
+		else if (versionNumber > inOrder.get(inOrder.size() - 2).getIdentifier())
+			return inOrder.get(inOrder.size() - 1);
 
 		return null;
 	}
