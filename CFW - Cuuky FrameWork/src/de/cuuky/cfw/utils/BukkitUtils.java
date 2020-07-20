70b64fff71fea041dd50478d9f59f1edcaf7f7d4
CuukyOfficial <just.cookie.jc@gmail.com>
2020-07-20 14:56:41 +0200

    Added saveTeleport

---
+++ CFW - Cuuky FrameWork/src/de/cuuky/cfw/utils/BukkitUtils.java
@@ -0,0 +1,16 @@
+package de.cuuky.cfw.utils;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class BukkitUtils {
+
+	public static void saveTeleport(Player player, Location location) {
+        while(!location.getChunk().isLoaded())
+            location.getChunk().load();
+       
+        Location blockunder = location.clone().add(0, -1, 0);
+        player.sendBlockChange(blockunder, blockunder.getBlock().getType(), (byte) 1);
+        player.teleport(location.clone().add(0, 1, 0));
+    }
+}
