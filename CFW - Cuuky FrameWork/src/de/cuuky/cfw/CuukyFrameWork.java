d50d90d783f0a6e3fd6604e7b30c6cc3cb335200
CuukyOfficial <just.cookie.jc@gmail.com>
2021-02-01 16:24:29 +0100

    Updated version to v0.4.4

--- CFW - Cuuky FrameWork/src/de/cuuky/cfw/CuukyFrameWork.java
+++ CFW - Cuuky FrameWork/src/de/cuuky/cfw/CuukyFrameWork.java
@@ -22,7 +22,7 @@ public class CuukyFrameWork {
 		ConfigurationSerialization.registerClass(CompatibleLocation.class);
 	}
 
-	private static final String NAME = "CuukyFrameWork", VERSION = "0.4.3", AUTHOR = "Cuuky";
+	private static final String NAME = "CuukyFrameWork", VERSION = "0.4.4", AUTHOR = "Cuuky";
 
 	/*
 	 * CFW - A Bukkit framework
