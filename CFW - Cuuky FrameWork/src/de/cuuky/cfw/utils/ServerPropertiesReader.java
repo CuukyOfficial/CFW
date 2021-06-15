3f106a5d4b6f052828f69e53fe911230bcc913e5
CuukyOfficial <just.cookie.jc@gmail.com>
2021-06-16 00:53:45 +0200

    Added basic 1.17 compatibility

--- CFW - Cuuky FrameWork/src/de/cuuky/cfw/utils/ServerPropertiesReader.java
+++ CFW - Cuuky FrameWork/src/de/cuuky/cfw/utils/ServerPropertiesReader.java
@@ -1,9 +1,11 @@
 package de.cuuky.cfw.utils;
 
 import de.cuuky.cfw.version.VersionUtils;
+import org.bukkit.Bukkit;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Properties;
 
@@ -26,14 +28,30 @@ public class ServerPropertiesReader {
         try {
             Class<?> mcServerClass = Class.forName(VersionUtils.getNmsClass() + ".MinecraftServer");
             Object mcServer = mcServerClass.getMethod("getServer").invoke(null);
-            Object propertyM = getFieldValue(mcServer.getClass().getField("propertyManager"), mcServer);
-            Object properties = getFieldValue(propertyM.getClass().getDeclaredField("properties"), propertyM);
-            if (!(properties instanceof Properties)) {
-                properties = getFieldValue(properties.getClass().getField("properties"), properties);
+            Object properties = null;
+            try {
+                Object propertyM = getFieldValue(mcServer.getClass().getField("propertyManager"), mcServer);
+                properties = getFieldValue(propertyM.getClass().getDeclaredField("properties"), propertyM);
+                if (!(properties instanceof Properties)) {
+                    properties = getFieldValue(properties.getClass().getField("properties"), properties);
+                }
+            } catch (NoSuchFieldException e) {
+                Method dedicatedServerPropMethod = Bukkit.getServer().getClass().getDeclaredMethod("getProperties");
+                dedicatedServerPropMethod.setAccessible(true);
+                Object dedicatedServerProp = dedicatedServerPropMethod.invoke(Bukkit.getServer());
+                Field[] fields = dedicatedServerProp.getClass().getFields();
+                for (Field field : fields) {
+                    if (!field.getType().equals(Properties.class))
+                        continue;
+
+                    field.setAccessible(true);
+                    properties = field.get(dedicatedServerProp);
+                    break;
+                }
             }
 
             return (Properties) properties;
-        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException | NoSuchFieldException e) {
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
 
