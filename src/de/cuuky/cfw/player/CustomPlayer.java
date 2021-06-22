bff6bef7c15ddff6b87ca85bc229785fa8b8a3d6
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2021-06-22 18:35:39 +0200

    removed deprecated method

--- src/de/cuuky/cfw/player/CustomPlayer.java
+++ src/de/cuuky/cfw/player/CustomPlayer.java
@@ -3,7 +3,6 @@ package de.cuuky.cfw.player;
 import org.bukkit.entity.Player;
 
 import de.cuuky.cfw.player.clientadapter.BoardUpdateHandler;
-import de.cuuky.cfw.player.connection.NetworkManager;
 
 public interface CustomPlayer {
 
@@ -12,9 +11,6 @@ public interface CustomPlayer {
 	public String getName();
 
 	public Player getPlayer();
-
-	@Deprecated
-	public NetworkManager getNetworkManager();
 
 	public String getLocale();
 
