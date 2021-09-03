9d90215dc40ec95549c4b683d4b4d4fcbb333b34
CuukyOfficial <just.cookie.jc@gmail.com>
2021-09-03 23:50:58 +0200

    Improved code and performance of placeholders

--- src/de/cuuky/cfw/configuration/placeholder/MessagePlaceholderManager.java
+++ src/de/cuuky/cfw/configuration/placeholder/MessagePlaceholderManager.java
@@ -3,7 +3,6 @@ package de.cuuky.cfw.configuration.placeholder;
 import de.cuuky.cfw.configuration.placeholder.placeholder.type.PlaceholderType;
 import de.cuuky.cfw.manager.FrameworkManager;
 import de.cuuky.cfw.manager.FrameworkManagerType;
-import javafx.util.Pair;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -14,87 +13,80 @@ import java.util.stream.Collectors;
 
 public class MessagePlaceholderManager extends FrameworkManager {
 
-	private int tickTolerance = 900;
+    private int tickTolerance = 900;
 
-	private final Map<PlaceholderType, List<MessagePlaceholder>> placeholders = new HashMap<>();
-	private final Map<PlaceholderType, Map<String, List<MessagePlaceholder>>> cachedRequests = new HashMap<>();
+    private final Map<PlaceholderType, List<MessagePlaceholder>> placeholders = new HashMap<>();
+    private final Map<PlaceholderType, Map<String, List<MessagePlaceholder>>> cachedRequests = new HashMap<>();
 
-	public MessagePlaceholderManager(JavaPlugin instance) {
-		super(FrameworkManagerType.PLACEHOLDER, instance);
-	}
+    public MessagePlaceholderManager(JavaPlugin instance) {
+        super(FrameworkManagerType.PLACEHOLDER, instance);
+    }
 
-	private Pair<String, List<MessagePlaceholder>> replaceByList(String value, List<MessagePlaceholder> list, boolean check, Object... args) {
-		if (list == null) return new Pair<>(value, null);
+    private List<MessagePlaceholder> search(PlaceholderType type, String containing) {
+        return this.getPlaceholders(type).stream().filter(mp -> mp.containsPlaceholder(containing)).collect(Collectors.toList());
+    }
 
-		List<MessagePlaceholder> cached = new ArrayList<>();
-		for (MessagePlaceholder pmp : list) {
-			if (check) {
-				if (!pmp.containsPlaceholder(value)) continue;
-				cached.add(pmp);
-			}
+    private String replaceByList(String value, List<MessagePlaceholder> list, Object... args) {
+        if (list == null) return null;
+        for (MessagePlaceholder pmp : list)
+            value = pmp.replacePlaceholder(value, args);
+        return value;
+    }
 
-			value = pmp.replacePlaceholder(value, args);
-		}
+    public String replacePlaceholders(String value, PlaceholderType type, Object... objects) {
+        Map<String, List<MessagePlaceholder>> reqs = cachedRequests.get(type);
+        List<MessagePlaceholder> placeholders;
+        if (reqs == null) reqs = new HashMap<>();
+        if ((placeholders = reqs.get(value)) == null) {
+            reqs.put(value, placeholders = this.search(type, value));
+            cachedRequests.put(type, reqs);
+        }
 
-		return new Pair<>(value, cached);
-	}
+        return replaceByList(value, placeholders, objects);
+    }
 
-	public String replacePlaceholders(String value, PlaceholderType type, Object... objects) {
-		Map<String, List<MessagePlaceholder>> reqs = cachedRequests.get(type);
-		if (reqs == null) reqs = new HashMap<>();
+    public MessagePlaceholder registerPlaceholder(MessagePlaceholder placeholder) {
+        if (placeholders.containsKey(placeholder.getType()))
+            placeholders.get(placeholder.getType()).add(placeholder);
+        else {
+            List<MessagePlaceholder> holders = new ArrayList<>();
+            holders.add(placeholder);
+            this.placeholders.put(placeholder.getType(), holders);
+        }
 
-		if (reqs.get(value) != null)
-			return replaceByList(value, reqs.get(value), false, objects).getKey();
-		else {
-			Pair<String, List<MessagePlaceholder>> result = replaceByList(value, getPlaceholders(type), true, objects);
-			if (result.getValue() != null) reqs.put(value, result.getValue());
-			cachedRequests.put(type, reqs);
-			return result.getKey();
-		}
-	}
+        return placeholder;
+    }
 
-	public MessagePlaceholder registerPlaceholder(MessagePlaceholder placeholder) {
-		if (placeholders.containsKey(placeholder.getType()))
-			placeholders.get(placeholder.getType()).add(placeholder);
-		else {
-			List<MessagePlaceholder> holders = new ArrayList<>();
-			holders.add(placeholder);
-			this.placeholders.put(placeholder.getType(), holders);
-		}
+    public MessagePlaceholder unregisterPlaceholder(MessagePlaceholder placeholder) {
+        if (placeholders.containsKey(placeholder.getType()))
+            placeholders.get(placeholder.getType()).remove(placeholder);
 
-		return placeholder;
-	}
+        return placeholder;
+    }
 
-	public MessagePlaceholder unregisterPlaceholder(MessagePlaceholder placeholder) {
-		if (placeholders.containsKey(placeholder.getType()))
-			placeholders.get(placeholder.getType()).remove(placeholder);
+    public void clear() {
+        for (PlaceholderType type : placeholders.keySet())
+            this.placeholders.get(type).forEach(MessagePlaceholder::clearValue);
+        this.cachedRequests.clear();
+    }
 
-		return placeholder;
-	}
+    public int getTickTolerance() {
+        return this.tickTolerance;
+    }
 
-	public void clear() {
-		for (PlaceholderType type : placeholders.keySet())
-			this.placeholders.get(type).forEach(MessagePlaceholder::clearValue);
-		this.cachedRequests.clear();
-	}
+    public void setTickTolerance(int tickTolerance) {
+        this.tickTolerance = tickTolerance;
+    }
 
-	public int getTickTolerance() {
-		return this.tickTolerance;
-	}
+    public Map<PlaceholderType, List<MessagePlaceholder>> getPlaceholders() {
+        return this.placeholders;
+    }
 
-	public void setTickTolerance(int tickTolerance) {
-		this.tickTolerance = tickTolerance;
-	}
+    public List<MessagePlaceholder> getAllPlaceholders() {
+        return this.placeholders.keySet().stream().flatMap(type -> this.placeholders.get(type).stream()).collect(Collectors.toList());
+    }
 
-	public Map<PlaceholderType, List<MessagePlaceholder>> getPlaceholders() {
-		return this.placeholders;
-	}
-
-	public List<MessagePlaceholder> getAllPlaceholders() {
-		return this.placeholders.keySet().stream().flatMap(type -> this.placeholders.get(type).stream()).collect(Collectors.toList());
-	}
-
-	public List<MessagePlaceholder> getPlaceholders(PlaceholderType type) {
-		return placeholders.get(type);
-	}
+    public List<MessagePlaceholder> getPlaceholders(PlaceholderType type) {
+        return placeholders.get(type);
+    }
 }
