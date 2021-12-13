97cd8dd0c6c17937da097892f28806194bd656b6
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2021-12-13 18:44:39 +0100

    1.18.1 protocol version

--- src/de/cuuky/cfw/version/minecraft/MinecraftVersion.java
+++ src/de/cuuky/cfw/version/minecraft/MinecraftVersion.java
@@ -6,7 +6,7 @@ public enum MinecraftVersion {
 
 	// Protocol version numbers: https://wiki.vg/Protocol_version_numbers
 	
-	MINECRAFT_1_18(757, "1.18"),
+	MINECRAFT_1_18_1(757, "1.18-1.18.1"),
 	MINECRAFT_1_17_1(756, "1.17.1"),
 	MINECRAFT_1_17(755, "1.17"),
 	MINECRAFT_1_16_5(754, "1.16.4-1.16.5"),
