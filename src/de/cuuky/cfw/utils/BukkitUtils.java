bdc246e4a6e45cd262e485c7076bccb895215798
uvgsj <uvgsj@student.kit.edu>
2022-01-06 17:52:31 +0100

    Added get player functions

--- src/de/cuuky/cfw/utils/BukkitUtils.java
+++ src/de/cuuky/cfw/utils/BukkitUtils.java
@@ -1,7 +1,10 @@
 package de.cuuky.cfw.utils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+
+import java.util.UUID;
 
 public class BukkitUtils {
 
@@ -13,4 +16,13 @@ public class BukkitUtils {
 		player.sendBlockChange(blockunder, blockunder.getBlock().getType(), (byte) 1);
 		player.teleport(location.clone().add(0, 1, 0));
 	}
+
+    public static Player getPlayer(String name) throws Exception {
+        return Bukkit.getPlayer(getUUID(name));
+    }
+
+    public static UUID getUUID(String name) throws Exception {
+        Player player = Bukkit.getPlayer(name);
+        return player == null ? UUIDUtils.getUUID(name) : player.getUniqueId();
+    }
 }
