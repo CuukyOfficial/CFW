5414364c240b02769752accd636c9b48ade8ea93
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2022-02-20 15:29:51 +0100

    Version 0.6.8

--- src/de/cuuky/cfw/CuukyFrameWork.java
+++ src/de/cuuky/cfw/CuukyFrameWork.java
@@ -22,7 +22,7 @@ public class CuukyFrameWork {
 		ConfigurationSerialization.registerClass(CompatibleLocation.class);
 	}
 
-	private static final String NAME = "CuukyFrameWork", VERSION = "0.6.7", AUTHOR = "Cuuky";
+	private static final String NAME = "CuukyFrameWork", VERSION = "0.6.8", AUTHOR = "Cuuky";
 
 	/*
 	 * CFW - A Bukkit framework
